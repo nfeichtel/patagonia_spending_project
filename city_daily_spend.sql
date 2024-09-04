98efd91ea513c7804143cc052db6c74224f3e8be
Nick Feichtel <nfeichtel@gmail.com>
2024-09-04 08:56:15 -0700

    create daily spend

---
+++ city_daily_spend.sql
@@ -0,0 +1,19 @@
+--city daily spend
+WITH total_usd AS
+    (
+    SELECT 
+        city,
+        ROUND(SUM(amount_usd)::numeric, 2) AS total_spend
+    FROM patagonia_spending
+    GROUP BY city
+    ORDER BY SUM(amount_usd) DESC
+    )
+
+SELECT
+    tu.city,
+    tu.total_spend,
+    ROUND(tu.total_spend/pd.days, 2) AS daily_spend
+FROM total_usd tu 
+JOIN patagonia_days pd 
+    ON tu.city = pd.city
+ORDER BY daily_spend DESC
